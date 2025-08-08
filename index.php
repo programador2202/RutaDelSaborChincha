e9386b64dfa87a4a0d81ab0947072771b92b4ad5
Aimar Alexander Contreras Carrillo <alexander2000contreras@gmail.com>
2025-08-08 10:12:35 -0500

    Actualiado

--- index.php
+++ index.php
@@ -365,6 +365,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <script>
+  
 document.addEventListener("DOMContentLoaded", function () {
   const selectCategoria = document.querySelector('select[name="categoria"]');
 
